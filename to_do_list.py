41e5a5cd46b2158464167ea1d058ec01fbeb0c07
villank2 <kylevillanueva1212@gmail.com>
2019-11-25 03:20:29 +0000

    update

--- to_do_list.py
+++ to_do_list.py
@@ -1,5 +1,6 @@
 import sys
 from os import system
+import time
 def main():
     print("Welcome to your Todo list app.\nEnter your name to continue.")
     cmd = input()
@@ -10,14 +11,45 @@ def main():
         show_actions()
         cmd = input()
         if(cmd=="1"):
-            todo.add_task()
-
-        
+            print("Enter date in dd/mm/yyyy format")
+            d,m,y = input().split('/')
+            date = Date(d,m,y)
+            print("Enter start time in hh:mm format")
+            h,m = input().split(":")
+            stime = Time(h,m)
+            print("Enter duration in hh:mm format")
+            h,m = input().split(":")
+            dtime = Time(h,m)
+            vtask = Task(d,stime,dtime)
+            li = get_names()
+            vtask.add_people(li)
+            todo.add(vtask)
+        elif(cmd=="2"):
+            print("Enter date in dd/mm/yyyy format")
+            d,m,y = input().split('/')
+            date = Date(d,m,y)
+            print("Enter start time in hh:mm format")
+            h,m = input().split(":")
+            stime = Time(h,m)
+            print("Enter location")
+            loc = input()
+            vevent = Event(date,stime,loc)
+            todo.add(vevent)
+        elif(cmd=="3"):
+            tmp = todo.rm()
+            if tmp.type == "Task":
+                print("{} {} {} {}".format(tmp.date,tmp.start_time,tmp.duration,tmp.people))
+            else:
+                print("{} {} {}".format(tmp.date,tmp.start_time,tmp.location))   
+            time.sleep(3)
+        elif(cmd=="4"):
+            sys.exit()
+            
 def get_names():
     print("Enter names")
     li =[]
     name = input()
-    while name != "\n":
+    while name != "":
         li.append(name)
         name=input()
     return li
@@ -36,62 +68,24 @@ class MyQueue(object):
     def __init__(self):
         self.q = []
     def is_empty(self):
-        return len(self.q==0)
+        return self.q == []
     def enq(self,item):
         print("adding to queue")
-        self.q.append(item)
-    def deq():
-        ret = self.q.pop(0) #remove head
-        return ret
-    def __str__(self):
-        if self.is_empty:
-            return "Nothing to do~"
-        else:
-            return "Sise of to do list : {}\n".format(len(self.q))
-
-class ToDoList(MyQueue):
-
-    def __init__(self,name):
-        super().__init__()
-        self.name = name
-    
-    def add_task(self):
-        stop = False
-        while not(stop):
-            print("Enter a valid date in form dd/mm/yyyy")
-            d,m,y = input().split("/")
-            print(d,m,y)
-            date = Date(d,m,y)
-            valid = date.validate()
-            print(valid)
-            if valid:
-                stop = True
-        stop = False
-        while not(stop):
-            print("Enter a valid start time in form hh:mm")
-            h,m = input().split(':')
-            time = Time(h,m)
-            valid = time.validate()
-            if valid:
-                stop = True
-        stop = False
-        while not(stop):
-            try:
-                print("Enter a valid time duration in form hh:mm")
-                h,m = input().split(':')
-                dtime = Time(h,m)
-                valid = dtime.validate()
-                if valid:
-                    stop = True
-            except: 
-        var_task = Task(date,time,dtime)
-        names = get_names()
-        var_task.add_people(names)
-        super().enq(var_task)
-        
+        self.q.insert(0,item)
     def deq(self):
-        ret = super().deq()
-        print(ret)
+        ret = self.q.pop()
+        return ret
+    
+
+class ToDoList():
+    def __init__(self,name):
+        self.name = name
+        self.q = MyQueue()
+    def add(self,item):
+        self.q.enq(item)
+    def rm(self):
+        return self.q.deq()
+    
 class Date():
 
     months = range(1,13)
@@ -107,7 +101,7 @@ class Date():
             return 0
         if self.day not in self.days:
             return 0
-        if len(str(self.year)) > 4:
+        if len(str(self.year)) != 4:
             return 0
         return 1
 
@@ -128,7 +122,7 @@ class Time():
             return 0
         return 1
     def __str__(self):
-        return "{%02d:%02d}".format(self.hours,self.mins)
+        return "{}:{}".format(self.hour,self.min)
 
 class GeneralType():
     
@@ -144,19 +138,20 @@ class Task(GeneralType):
         super().__init__(date,start_time)
         self.duration = duration
         self.people = []
+        self.type = "Task"
     def add_people(self,li):
         for name in li:
             self.people.append(name)
 
     def __str__(self):
         x = super().__str__()
-        return "{}\n{}\n".format(x,self.duration,str(self.people))
+        return "{}\n{}\n{}\n".format(x,self.duration,str(self.people))
 
 class Event(GeneralType):
     def __init__(self,date,start_time,location):
         super().__init__(date,start_time)
         self.location = location
-    
+        self.type = "Event"
     def __str__(self):
         x = super().__str__()
         return "{}\n{}\n".format(x,self.location)
